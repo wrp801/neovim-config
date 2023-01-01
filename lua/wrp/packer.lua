94a75adb3dc1fbc3c117c8a663032389ab69e716
wrp801 <wespoulsen@gmail.com>
2023-01-01 13:08:19 -0700

    Removed file tree

--- lua/wrp/packer.lua
+++ lua/wrp/packer.lua
@@ -1,12 +1,12 @@
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+    local fn = vim.fn
+    local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
 end
 
 local packer_bootstrap = ensure_packer()
@@ -19,50 +19,58 @@ local packer_bootstrap = ensure_packer()
 -- vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.0',
-		-- or                            , branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
-	use { "EdenEast/nightfox.nvim", as = 'nightfox', 
---	config = function() vim.cmd('colorscheme nightfox') end
-}
-	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-	use 'ThePrimeagen/harpoon' -- for hopping between files
-	use 'mbbill/undotree'
-	use 'tpope/vim-fugitive' -- for git stuff
-	use {
-		'VonHeikemen/lsp-zero.nvim',
-		requires = {
-			-- LSP Support
-			{'neovim/nvim-lspconfig'},
-			{'williamboman/mason.nvim'},
-			{'williamboman/mason-lspconfig.nvim'},
-
-			-- Autocompletion
-			{'hrsh7th/nvim-cmp'},
-			{'hrsh7th/cmp-buffer'},
-			{'hrsh7th/cmp-path'},
-			{'saadparwaiz1/cmp_luasnip'},
-			{'hrsh7th/cmp-nvim-lsp'},
-			{'hrsh7th/cmp-nvim-lua'},
-
-			-- Snippets
-			{'L3MON4D3/LuaSnip'},
-			{'rafamadriz/friendly-snippets'},
-		}
-}
-    -- for aesthetics
-    use 'vim-airline/vim-airline'
-    use 'vim-airline/vim-airline-themes'
-    -- for comments 
     use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.0',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+    use { "EdenEast/nightfox.nvim", as = 'nightfox', 
+    --	config = function() vim.cmd('colorscheme nightfox') end
+}
+use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+use 'ThePrimeagen/harpoon' -- for hopping between files
+use 'mbbill/undotree'
+use 'tpope/vim-fugitive' -- for git stuff
+use {
+    'VonHeikemen/lsp-zero.nvim',
+    requires = {
+        -- LSP Support
+        {'neovim/nvim-lspconfig'},
+        {'williamboman/mason.nvim'},
+        {'williamboman/mason-lspconfig.nvim'},
+
+        -- Autocompletion
+        {'hrsh7th/nvim-cmp'},
+        {'hrsh7th/cmp-buffer'},
+        {'hrsh7th/cmp-path'},
+        {'saadparwaiz1/cmp_luasnip'},
+        {'hrsh7th/cmp-nvim-lsp'},
+        {'hrsh7th/cmp-nvim-lua'},
+
+        -- Snippets
+        {'L3MON4D3/LuaSnip'},
+        {'rafamadriz/friendly-snippets'},
+    }
+}
+-- for aesthetics
+use 'vim-airline/vim-airline'
+use 'vim-airline/vim-airline-themes'
+-- for comments 
+use {
     'numToStr/Comment.nvim',
     config = function()
         require('Comment').setup()
     end
 }
+-- file tree 
+-- use {
+--     'nvim-tree/nvim-tree.lua',
+--     requires = {
+--         'nvim-tree/nvim-web-devicons', -- optional, for file icons
+--     },
+--     tag = 'nightly' -- optional, updated every week. (see issue #1193)
+-- }
 end)
