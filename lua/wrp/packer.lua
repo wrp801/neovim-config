892006f9609ae15608a7c087abb832166ebd187d
wrp801 <wespoulsen@gmail.com>
2023-05-14 20:46:52 -0600

    Added Trouble plugin

--- lua/wrp/packer.lua
+++ lua/wrp/packer.lua
@@ -89,6 +89,17 @@ use {
     -- Uncomment next line if you want to follow only stable versions
     -- tag = "*"
 }
+use {
+  "folke/trouble.nvim",
+  requires = "nvim-tree/nvim-web-devicons",
+  config = function()
+    require("trouble").setup {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  end
+}
 
 -- file tree 
 -- use {
