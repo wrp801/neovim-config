500e61b3419de4aae33768b73cfc11e95cb3a1eb
wrp801 <wespoulsen@gmail.com>
2023-09-19 09:52:10 -0600

    fixed treesitter issue

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "c", "lua", "rust", "help", "python", "julia", "cmake", "go", "cpp","help"},
+  ensure_installed = { "c", "lua", "rust", "help", "python", "julia", "vim", "go", "cpp","help"},
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
