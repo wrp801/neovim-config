33423e203974ca5fc369b04d58ee649051c3eeec
wrp801 <wespoulsen@gmail.com>
2023-09-20 14:23:47 -0600

    updated lsp to include inline diagnostics

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -55,7 +55,23 @@ lsp.setup_nvim_cmp({
     end)
 
     lsp.setup()
-    vim.diagnostic.config({
-        virtual_text = true
-    })
 
+    local function setup_lsp_diags()
+        vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+        vim.lsp.diagnostic.on_publish_diagnostics,
+        {
+            virtual_text = true,
+            signs = true,
+            update_in_insert = false,
+            underline = true,
+        }
+        )
+    end
+
+    setup_lsp_diags()
+
+    -- vim.diagnostic.config({
+    --     virtual_text = true,
+    --
+    -- })
+    --
