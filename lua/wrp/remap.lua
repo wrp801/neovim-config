b7b9d27c7755e7e2e79dbb38c725463ab7461ed0
wrp801 <wespoulsen@gmail.com>
2023-09-24 21:29:43 -0600

    added telescope git commands

--- lua/wrp/remap.lua
+++ lua/wrp/remap.lua
@@ -81,4 +81,8 @@ vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ")});
 end)
 
-
+-- telescope git commands (not on youtube nvim video)
+vim.keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<cr>") -- list all git commits (use <cr> to checkout) ["gc" for git commits]
+vim.keymap.set("n", "<leader>gfc", "<cmd>Telescope git_bcommits<cr>") -- list git commits for current file/buffer (use <cr> to checkout) ["gfc" for git file commits]
+vim.keymap.set("n", "<leader>gb", "<cmd>Telescope git_branches<cr>") -- list git branches (use <cr> to checkout) ["gb" for git branch]
+vim.keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<cr>") -- list current changes per file with diff preview ["gs" for git status]
