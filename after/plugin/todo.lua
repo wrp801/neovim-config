c2032aeeb23478476b6f275d69abd440de9b0a41
wrp801 <wespoulsen@gmail.com>
2024-09-12 18:07:32 -0600

    added additional todo keymaps

--- after/plugin/todo.lua
+++ after/plugin/todo.lua
@@ -2,3 +2,5 @@ require('todo-comments').setup()
 
 vim.keymap.set('n', '<leader>tt', ':TodoTelescope<CR>', {desc = "List of TODO's in telescope format"})
 vim.keymap.set('n', '<leader>tx', ':TodoTrouble<CR>', {desc = 'List of trouble items'})
+vim.keymap.set('n', '<leader>td', ':TodoTelescope keywords=TODO,NOTE <CR>', { desc = 'List the TODO and NOTE items'})
+vim.keymap.set('n', '<leader>tb', ':TodoTelescope keywords=FIXME,BUG,FIX <CR>', {desc = 'List the FIXME, BUG and FIX items'})
